791b2ad715008466efad2e13c6991e973596e5f8
viator3m <viator3m@gmail.com>
2022-09-17 17:13:55 +0300

    edit allowed_hosts

--- infra_project/infra_project/settings.py
+++ infra_project/infra_project/settings.py
@@ -25,11 +25,7 @@ SECRET_KEY = 'k9gwcfq01xszx2(_4e#gc3u$rt=)y6ivx=$wt+5&-^ktk4dg4e'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    '84.252.137.116',
-    'localhost',
-    '127.0.0.1'
-]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
